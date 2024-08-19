de643f0748c179afdf51eb532302c41fa322a18c
NikitaPred <nikita.predtechensky@gmail.com>
2024-08-19 16:11:45 -0700

    sizing issues

--- src/components/Account/PriceTable.jsx
+++ src/components/Account/PriceTable.jsx
@@ -2,35 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { Auth } from 'aws-amplify';
 import './PriceTable.css';
 
-const StripePricingTable = ({ onBack }) => {
+const StripePricingTable = () => {
   const [userEmail, setUserEmail] = useState('');
 
   useEffect(() => {
-    const fetchUserEmail = async () => {
-      try {
-        const user = await Auth.currentAuthenticatedUser();
-        const { attributes } = user;
-        setUserEmail(attributes.email);
-        console.log(userEmail);
-      } catch (error) {
-        console.error('Error fetching user email:', error);
-      }
-    };
+    // Add Stripe Pricing Table script
+    const script = document.createElement('script');
+    script.src = 'https://js.stripe.com/v3/pricing-table.js';
+    script.async = true;
+    document.head.appendChild(script);
+    return () => document.head.removeChild(script);
+  }, []);
 
-    fetchUserEmail();
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(user => setUserEmail(user.attributes.email))
+      .catch(error => console.error('Error fetching user email:', error));
   }, []);
 
   return (
-    <>
-      <button onClick={onBack} className="back-button">Back to Account Management</button>
-      <div className="pricing-table-container">
-        <stripe-pricing-table
-          pricing-table-id="prctbl_1PnBmnGW9XarT1STkRJVEynp"
-          publishable-key="pk_test_51OjRoLGW9XarT1STitbive9uLi8wemvrnBWCom1352qFV0pu87IzzFmE3eO1zxy4Qb8bA3xMwMojxMNUdVFoRnqX00j72cKeAO"
-          customer-email={userEmail}
-        />
-      </div>
-    </>
+    <div className="pricing-table-container">
+      <stripe-pricing-table
+        pricing-table-id="prctbl_1PnBmnGW9XarT1STkRJVEynp"
+        publishable-key="pk_test_51OjRoLGW9XarT1STitbive9uLi8wemvrnBWCom1352qFV0pu87IzzFmE3eO1zxy4Qb8bA3xMwMojxMNUdVFoRnqX00j72cKeAO"
+        customer-email={userEmail}
+      />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
