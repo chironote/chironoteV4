0ad52bcc70e9eeeb7ac7db10aa2505d1800aa6e3
chironote <development@chironote.ai>
2026-09-02 14:14:47 -0700

    Revert "Fix blog post export"
    This reverts commit 0b6c36661966788c85fe4fc13a8d2502e08a4440.

--- src/components/Blog/Post 6.jsx
+++ src/components/Blog/Post 6.jsx
@@ -33,9 +33,4 @@ function Post6Content() {
   );
 }
 
-const post = {
-  ...metadata,
-  component: Post6Content,
-};
-
-export default post;
+export default { ...metadata, component: Post6Content };
